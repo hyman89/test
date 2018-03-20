a29dc16816c26b61181a264ad7c73a34f1efdc6e
hyman <625446618@qq.com>
2018-03-20 23:02:16 +0800

    test

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by hyman on 2018/3/20.
+//  Copyright © 2018年 kabaojia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
